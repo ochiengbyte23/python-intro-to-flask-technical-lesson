543fe04e59403c30ebcc43cd260deda99080fc24
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-03-09 23:13:29 +0300

    added variable routes

--- server/app.py
+++ server/app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Welcome to my page!<h1>'
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}<h1>'
